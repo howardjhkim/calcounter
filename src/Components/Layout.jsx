edba12129d266cf520131a0e6acdd71ee49210ed
howardjhkim <howardjhkim@gmail.com>
2023-04-25 18:01:54 -0700

    Updates

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,13 +3,20 @@ import {Outlet} from "react-router-dom"
 
 import Header from "./Header"
 import Footer from "./Footer"
+import VerticalNavbar from "./VerticalNavbar"
 
 export default function Layout() {
     return(
         <div className="site-wrapper">
-           <Header />
-           <Outlet />
-           <Footer />
+            <div>
+                <VerticalNavbar />
+            </div>
+
+            <div className="components">
+                <Header />
+                <Outlet />
+                <Footer />
+            </div>
         </div>
     )
 }
---
+++ src/Components/VerticalNavbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function VerticalNavbar() {
+    
+    return (
+        <div className="vertical-nav-bar">
+            <span className="vertical-nav-bar-li-title">Menu</span>
+            <ul>
+                <li>Home</li>
+                <li>Workouts</li>
+                <li>Weight Calculator</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    )
+}
